c651fb8551be436a8736a19b3ad00c41c228ab46
Jeff Decker <jeffpdecker@gmail.com>
2020-08-28 09:24:12 -0700

    commit stable version before I start mucking around with it

--- plotData.py
+++ plotData.py
@@ -7,40 +7,35 @@ import pandas
 import numpy
 
 #pathToFile = 'logfile-20200815 10-59-29.txt' ; comments = 'test file with real datetime objects'
-
-
 #pathToFile = 'logFile-logfile-20200806 12-06-08.csv'; comments = 'baseline 1/3, antenna on electrical panel and paint can (pic on phone)'
 #pathToFile = 'logFile-logfile-20200807 13-52-13.csv'; comments = 'baseline 3/3'
-
-
 #pathToFile = 'logfile-20200808 10-01-20.txt' # eh?
 #pathToFile = 'logfile-20200808 22-32-43.txt'; comments = '1/4 removed external antennae; internal antennae only'
 #pathToFile = 'logfile-20200809 10-33-08.txt'; comments = '2/4 of no external antennae'
 #pathToFile = 'logfile-20200809 23-08-07.txt'; comments = '3/4 no external anteannae PM -> AM'
 #pathToFile = 'logfile-20200810 11-32-53.txt'; comments = '4/4 no external antennae AM -> PM'
-
 #pathToFile = 'logfile-20200811 19-30-56.txt'; comments = '1/? baseline; antenna on panel and paint can pic (8/6)'
 #pathToFile = 'logfile-20200812 08-11-24.txt'; comments = '2/? baseline; AM -> PM'
 #pathToFile = 'logfile-20200812 20-36-01.txt'; comments = '3/? baseline PM - > AM'
 #pathToFile = 'logfile-20200813 08-59-11.txt'; comments = '4/? baseline AM -> PM'
 #pathToFile = 'logfile-20200813 22-50-28.txt'; comments = '5/5 baseline PM -> AM'
+#pathToFile = 'logfile-20200814 18-40-18.txt'; comments = '1/1 antennae on tablesaw and paint can; before real datetimes'
 
-#pathToFile = 'logfile-20200814 18-40-18.txt'; comments = '1/? antennae on tablesaw and paint can'
-#pathToFile = 'logfile-20200815 11-02-38.txt'; comments = '2/? antennae on tablesaw and paint can; first with real datetimes'
-#pathToFile = 'logfile-20200816 10-11-21.txt'; comments = '3/? antennae on tablesaw and paint can; first with real datetimes'
-#pathToFile = 'logfile-20200817 07-43-27.txt'; comments = '4/? antennae on tablesaw and paint can; first with real datetimes'
 
-#pathToFile = 'logfile-20200817 19-26-06.txt'; comments = '1/? no external antennae'
-#pathToFile = 'logfile-20200818 12-45-21.txt'; comments = '2/? no external antennae'
-#pathToFile = 'logfile-20200819 11-59-43.txt'; comments = '3/? no external antennae'
-#pathToFile = 'logfile-20200819 22-27-55.txt'; comments = '4/? no external antennae'
-#pathToFile = 'logfile-20200821 09-26-18.txt'; comments = '5/? no external antennae'
-#pathToFile = 'logfile-20200821 21-21-26.txt'; comments = '6/? no external antennae'
-
-#pathToFile = 'logfile-20200822 10-06-56.txt'; comments = '1/? antennae on paint can & electrical panel'
-#pathToFile = 'logfile-20200822 23-54-03.txt'; comments = '2/? antennae on paint can & electrical panel'
-#pathToFile = 'logfile-20200823 11-54-14.txt'; comments = '3/? antennae on paint can & electrical panel'
-pathToFile = 'logfile-20200824 00-10-26.txt'; comments = '4/? antennae on paint can & electrical panel'
+#pathToFile = 'logfile-20200815 11-02-38.txt'; comments = '2/3 antennae on tablesaw and paint can; first with real datetimes'
+#pathToFile = 'logfile-20200816 10-11-21.txt'; comments = '3/3 antennae on tablesaw and paint can; first with real datetimes'
+#pathToFile = 'logfile-20200817 07-43-27.txt'; comments = '4/3 antennae on tablesaw and paint can; first with real datetimes'
+#pathToFile = 'logfile-20200817 19-26-06.txt'; comments = '1/6 no external antennae'
+#pathToFile = 'logfile-20200818 12-45-21.txt'; comments = '2/6 no external antennae'
+#pathToFile = 'logfile-20200819 11-59-43.txt'; comments = '3/6 no external antennae'
+#pathToFile = 'logfile-20200819 22-27-55.txt'; comments = '4/6 no external antennae'
+#pathToFile = 'logfile-20200821 09-26-18.txt'; comments = '5/6 no external antennae'
+#pathToFile = 'logfile-20200821 21-21-26.txt'; comments = '6/6 no external antennae'
+#pathToFile = 'logfile-20200822 10-06-56.txt'; comments = '1/5 antennae on paint can & electrical panel'
+#pathToFile = 'logfile-20200822 23-54-03.txt'; comments = '2/5 antennae on paint can & electrical panel'
+#pathToFile = 'logfile-20200823 11-54-14.txt'; comments = '3/5 antennae on paint can & electrical panel'
+#pathToFile = 'logfile-20200824 00-10-26.txt'; comments = '4/5 antennae on paint can & electrical panel'
+pathToFile = 'logfile-20200824 22-59-57.txt'; comments = '5/5 antennae on paint can & electrical panel'
 
 
 with open(pathToFile, 'r') as file:
